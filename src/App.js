0515a0467ab9c33b2ab8021ceca19e0e0bc5206d
Shubham <shubham998845@gmail.com>
2021-08-09 23:25:36 +0530

    Product component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>product list</main>
-      <footer>All rights reserved</footer>
-    </div>
-  );
+import React, { Component } from 'react';
+import Products from './components/Products';
+import data from './data.json';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All rights reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
