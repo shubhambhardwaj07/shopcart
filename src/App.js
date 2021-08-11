155890463b6182c6f6fd4205ec6a9df8e1197596
Shubham <shubham998845@gmail.com>
2021-08-11 21:42:21 +0530

    Cart component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
 import React, { Component } from 'react';
 import Products from './components/Products';
+import Filter from './components/Filter';
 import data from './data.json';
+import Cart from './components/Cart';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: '',
       sort: '',
     };
   }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+  };
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems });
+  };
+
+  sortProducts = (event) => {
+    console.log(event.target.value);
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === 'lowest'
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === 'highest'
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+
+  filterProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === '') {
+      this.setState({ size: event.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
+
   render() {
     return (
       <div className="grid-container">
@@ -20,9 +83,24 @@ class App extends Component {
         <main>
           <div className="content">
             <div className="main">
-              <Products products={this.state.products}></Products>
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              ></Products>
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
+            </div>
           </div>
         </main>
         <footer>All rights reserved</footer>
